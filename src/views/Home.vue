4e76b73e51d9e3df6851ce35e57e0c3c2d2774e6
Alexo <alexander.c.owusu@gmail.com>
2021-07-24 23:28:24 +0100

    landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-card class="box-card">
+      <el-button type="primary" round @click="$router.push('Play')"
+        >Play the game</el-button
+      >
+
+      <div class="about">
+        <el-link type="info" @click="$router.push('About')">About</el-link>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
+  components: {},
 };
 </script>
+<style lang="sass" scoped>
+.home
+  min-height: 100vh
+  min-width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+  background: linear-gradient(to right, #b3ffab, #12fff7)
+  .box-card
+    max-height: 200px
+.about
+  margin-top: 1em
+</style>
